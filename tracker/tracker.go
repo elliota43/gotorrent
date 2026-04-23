82e6bb6898ddfbf7ee77711d20b62f4c47d8f96c
Elliot Anderson <elliot.anderson43@yahoo.com>
2026-04-23 17:41:44 -0400

    tracker url

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,7 +1,11 @@
 package tracker
 
 import (
+	"fmt"
 	"net"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/elliota43/gotorrent/torrent"
 )
@@ -44,10 +48,66 @@ func NewAnnounceRequest(meta *torrent.TorrentMeta, peerID [20]byte, port uint16)
 	}
 }
 
-func NewPeerID() string {
-	return PeerID
+func NewPeerID() [20]byte {
+	var id [20]byte
+	copy(id[:], PeerID)
+	return id
 }
 
 func NewPort() uint16 {
 	return Port
+}
+
+func (ar AnnounceRequest) GetURL(announceURL string) (string, error) {
+	base, err := url.Parse(announceURL)
+	if err != nil {
+		return "", err
+	}
+
+	q := buildTrackerQuery(ar)
+
+	if base.RawQuery == "" {
+		base.RawQuery = q
+	} else {
+		base.RawQuery = base.RawQuery + "&" + q
+	}
+	return base.String(), nil
+}
+
+func buildTrackerQuery(ar AnnounceRequest) string {
+	params := []string{
+		"info_hash=" + escapeBytes(ar.InfoHash[:]),
+		"peer_id=" + escapeBytes(ar.PeerID[:]),
+		"port=" + strconv.FormatUint(uint64(ar.Port), 10),
+		"uploaded=" + strconv.FormatInt(ar.Uploaded, 10),
+		"downloaded=" + strconv.FormatInt(ar.Downloaded, 10),
+		"left=" + strconv.FormatInt(ar.Left, 10),
+	}
+
+	if ar.Compact {
+		params = append(params, "compact=1")
+	} else {
+		params = append(params, "compact=0")
+	}
+
+	if ar.Event != "" {
+		params = append(params, "event="+url.QueryEscape(ar.Event))
+	}
+
+	return strings.Join(params, "&")
+}
+
+func escapeBytes(b []byte) string {
+	var sb strings.Builder
+	for _, c := range b {
+		if (c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z') ||
+			(c >= '0' && c <= '9') ||
+			c == '.' || c == '-' || c == '_' || c == '~' {
+			sb.WriteByte(c)
+		} else {
+			sb.WriteString(fmt.Sprintf("%%%02X", c))
+		}
+	}
+	return sb.String()
 }
--- tracker/tracker_test.go
+++ tracker/tracker_test.go
@@ -1,19 +1,227 @@
 package tracker
 
-import "testing"
+import (
+	"net/url"
+	"strings"
+	"testing"
+)
 
-func TestNewPeerID(t *testing.T) {
-	id := NewPeerID()
+func TestAnnounceRequest_GetURL_BuildsExpectedQuery(t *testing.T) {
+	var infoHash [20]byte
+	var peerID [20]byte
 
-	if id != PeerID {
-		t.Fatalf("expected %q, got %q", PeerID, id)
+	copy(infoHash[:], []byte("12345678901234567890"))
+	copy(peerID[:], []byte("-GT0001-123456789012"))
+
+	ar := AnnounceRequest{
+		InfoHash:   infoHash,
+		PeerID:     peerID,
+		Port:       6881,
+		Uploaded:   0,
+		Downloaded: 0,
+		Left:       999,
+		Compact:    true,
+		Event:      "started",
+	}
+
+	got, err := ar.GetURL("http://tracker.example.com/announce")
+	if err != nil {
+		t.Fatalf("GetURL() error = %v", err)
+	}
+
+	u, err := url.Parse(got)
+	if err != nil {
+		t.Fatalf("url.Parse() error = %v", err)
+	}
+
+	if u.Scheme != "http" {
+		t.Fatalf("scheme = %q, want %q", u.Scheme, "http")
+	}
+
+	if u.Host != "tracker.example.com" {
+		t.Fatalf("host = %q, want %q", u.Host, "tracker.example.com")
+	}
+
+	if u.Path != "/announce" {
+		t.Fatalf("path = %q, want %q", u.Path, "/announce")
+	}
+
+	query := u.RawQuery
+
+	assertQueryHasPrefix(t, query, "info_hash=")
+	assertQueryHasPrefix(t, query, "peer_id=")
+	assertQueryContains(t, query, "port=6881")
+	assertQueryContains(t, query, "uploaded=0")
+	assertQueryContains(t, query, "downloaded=0")
+	assertQueryContains(t, query, "left=999")
+	assertQueryContains(t, query, "compact=1")
+	assertQueryContains(t, query, "event=started")
+}
+
+func TestAnnounceRequest_GetURL_CompactFalse(t *testing.T) {
+	var infoHash [20]byte
+	var peerID [20]byte
+
+	copy(infoHash[:], []byte("12345678901234567890"))
+	copy(peerID[:], []byte("-GT0001-123456789012"))
+
+	ar := AnnounceRequest{
+		InfoHash:   infoHash,
+		PeerID:     peerID,
+		Port:       6881,
+		Uploaded:   10,
+		Downloaded: 20,
+		Left:       30,
+		Compact:    false,
+	}
+
+	got, err := ar.GetURL("http://tracker.example.com/announce")
+	if err != nil {
+		t.Fatalf("GetURL() error = %v", err)
+	}
+
+	u, err := url.Parse(got)
+	if err != nil {
+		t.Fatalf("url.Parse() error = %v", err)
+	}
+
+	query := u.RawQuery
+	assertQueryContains(t, query, "compact=0")
+	assertQueryContains(t, query, "uploaded=10")
+	assertQueryContains(t, query, "downloaded=20")
+	assertQueryContains(t, query, "left=30")
+}
+
+func TestAnnounceRequest_GetURL_OmitsEmptyEvent(t *testing.T) {
+	var infoHash [20]byte
+	var peerID [20]byte
+
+	copy(infoHash[:], []byte("12345678901234567890"))
+	copy(peerID[:], []byte("-GT0001-123456789012"))
+
+	ar := AnnounceRequest{
+		InfoHash: infoHash,
+		PeerID:   peerID,
+		Port:     6881,
+		Left:     100,
+		Compact:  true,
+		Event:    "",
+	}
+
+	got, err := ar.GetURL("http://tracker.example.com/announce")
+	if err != nil {
+		t.Fatalf("GetURL() error = %v", err)
+	}
+
+	u, err := url.Parse(got)
+	if err != nil {
+		t.Fatalf("url.Parse() error = %v", err)
+	}
+
+	if strings.Contains(u.RawQuery, "event=") {
+		t.Fatalf("query unexpectedly contains event: %q", u.RawQuery)
 	}
 }
 
-func TestNewPort(t *testing.T) {
-	port := NewPort()
+func TestAnnounceRequest_GetURL_PreservesExistingQuery(t *testing.T) {
+	var infoHash [20]byte
+	var peerID [20]byte
 
-	if port != Port {
-		t.Fatalf("expected %d, got %d", Port, port)
+	copy(infoHash[:], []byte("12345678901234567890"))
+	copy(peerID[:], []byte("-GT0001-123456789012"))
+
+	ar := AnnounceRequest{
+		InfoHash: infoHash,
+		PeerID:   peerID,
+		Port:     6881,
+		Left:     100,
+		Compact:  true,
+	}
+
+	got, err := ar.GetURL("http://tracker.example.com/announce?foo=bar")
+	if err != nil {
+		t.Fatalf("GetURL() error = %v", err)
+	}
+
+	u, err := url.Parse(got)
+	if err != nil {
+		t.Fatalf("url.Parse() error = %v", err)
+	}
+
+	query := u.RawQuery
+	assertQueryContains(t, query, "foo=bar")
+	assertQueryContains(t, query, "port=6881")
+	assertQueryContains(t, query, "compact=1")
+}
+
+func TestAnnounceRequest_GetURL_InvalidBaseURL(t *testing.T) {
+	var infoHash [20]byte
+	var peerID [20]byte
+
+	ar := AnnounceRequest{
+		InfoHash: infoHash,
+		PeerID:   peerID,
+		Port:     6881,
+		Compact:  true,
+	}
+
+	_, err := ar.GetURL(":// bad url")
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
+
+func TestEscapeBytes_PercentEncodesReservedBytes(t *testing.T) {
+	in := []byte{
+		0x00,
+		0x7f,
+		'/',
+		'?',
+		'&',
+		'A',
+		'z',
+		'0',
+		'-',
+		'_',
+		'.',
+		'~',
+	}
+
+	got := escapeBytes(in)
+
+	wantParts := []string{
+		"%00",
+		"%7F",
+		"%2F",
+		"%3F",
+		"%26",
+		"A",
+		"z",
+		"0",
+		"-",
+		"_",
+		".",
+		"~",
+	}
+
+	for _, part := range wantParts {
+		if !strings.Contains(got, part) {
+			t.Fatalf("escapeBytes(%v) = %q, want it to contain %q", in, got, part)
+		}
+	}
+}
+
+func assertQueryContains(t *testing.T, query, want string) {
+	t.Helper()
+	if !strings.Contains(query, want) {
+		t.Fatalf("query = %q, want substring %q", query, want)
+	}
+}
+
+func assertQueryHasPrefix(t *testing.T, query, key string) {
+	t.Helper()
+
+	if !strings.Contains(query, key) {
+		t.Fatalf("query = %q, want key %q present", query, key)
 	}
 }
